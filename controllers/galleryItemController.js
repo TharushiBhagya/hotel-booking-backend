7a75b43102e5adb8dcc6cb24cf135f398ec7468a
Tharu <154546452+TharushiBhagya@users.noreply.github.com>
2024-10-19 23:50:20 +0530

    Update gallery item controller

--- controllers/galleryItemController.js
+++ controllers/galleryItemController.js
@@ -1,41 +1,40 @@
 import GalleryItem from "../models/galleryItem.js";
+import { isAdminValid } from "./userControllers.js";
 
-export function createGalleryItem(req,res){
-    const user=req.user
-    if(user==null){
-          res.status(403).json({
-            message:"Please login to create a gallery item"
-          })
-    return
+export function createGalleryItem(req, res) {
+   
+    if (!isAdminValid(req)) {
+        return res.status(403).json({
+            message: "You are not authorized to create a gallery item"
+        });
     }
-   if(user.type !="admin"){
-    res.status(403).json({
-        message:"You are not authorized to create a gallery item"
-    })
-    return
-   }
-    const galleryItem=req.body.item
-    const newGalleryItem=new GalleryItem(galleryItem)
-    newGalleryItem.save().then(
-        ()=>{
+
+    const galleryItem = req.body.item;
+    const newGalleryItem = new GalleryItem(galleryItem);
+
+    newGalleryItem.save()
+        .then(() => {
             res.json({
-                message:"Gallery item created successfully"
-            })
-        }
-    ).catch(
-        ()=>{
+                message: "Gallery item created successfully"
+            });
+        })
+        .catch(() => {
             res.status(500).json({
-                message:"Gallery item creation failed"
-            })
-        }
-    )
+                message: "Gallery item creation failed"
+            });
+        });
 }
-export function getGalleryItems(req,res){
-    GalleryItem.find().then(
-        (list)=>{
+
+export function getGalleryItems(req, res) {
+    GalleryItem.find()
+        .then((list) => {
             res.json({
-                list:list
-            })
-        }
-    )
+                list: list
+            });
+        })
+        .catch(() => {
+            res.status(500).json({
+                message: "Failed to retrieve gallery items"
+            });
+        });
 }
