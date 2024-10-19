8c7a685f7326c5f16831aafc558d414979b71b4c
Tharu <154546452+TharushiBhagya@users.noreply.github.com>
2024-10-19 23:53:39 +0530

    GallerItemRouter is updated for authentication purposes.

--- routes/galleryItemRouter.js
+++ routes/galleryItemRouter.js
@@ -2,10 +2,9 @@ import express from 'express';
 import { createGalleryItem, getGalleryItems } from '../controllers/galleryItemController.js'; 
 import { authenticateToken } from "../middleware/authenticateToken.js"; 
 
-const router = express.Router();
+const galleryItemRouter = express.Router();
 
-router.use(authenticateToken);
-router.post('/', createGalleryItem);
-router.get('/', getGalleryItems);
+galleryItemRouter.post('/', createGalleryItem);
+galleryItemRouter.get('/',authenticateToken,getGalleryItems);
 
-export default router;
+export default galleryItemRouter;
