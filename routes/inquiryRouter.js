5203ac3afc800c9c1c9f74df61630720ddcacd45
Tharu <154546452+TharushiBhagya@users.noreply.github.com>
2024-10-20 00:07:28 +0530

    Update inquiryRouter.js

--- routes/inquiryRouter.js
+++ routes/inquiryRouter.js
@@ -1,12 +1,13 @@
 import express from "express";
 import {createInquiry, getAllInquiries, getInquiryById, updateInquiryStatus, deleteInquiry} from "../controllers/inquiryController.js";
+import { authenticateToken } from "../middleware/authenticateToken.js"; 
 
 const InquiryRouter = express.Router();
 
 InquiryRouter.post("/", createInquiry);
-InquiryRouter.get("/", getAllInquiries);
-InquiryRouter.get("/:id", getInquiryById);
-InquiryRouter.put("/:id/status", updateInquiryStatus);
-InquiryRouter.delete("/:id", deleteInquiry);
+InquiryRouter.get("/", authenticateToken, getAllInquiries);
+InquiryRouter.get("/:id", authenticateToken, getInquiryById);
+InquiryRouter.put("/:id/status", authenticateToken, updateInquiryStatus);
+InquiryRouter.delete("/:id", authenticateToken, deleteInquiry);
 
 export default InquiryRouter;
