8392722a097b60cb4e34bd4b5fde3a2382043dd9
Tharu <154546452+TharushiBhagya@users.noreply.github.com>
2024-10-20 02:37:04 +0530

    Update userRouter with blocking spams

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,10 +1,12 @@
-import express from "express";
-import {postUsers,loginUser} from "../controllers/userControllers.js";
+import express from 'express';
+import { postUsers, loginUser, blockUser, unblockUser } from '../controllers/userController.js';
+import { authenticateToken } from "../middleware/authenticateToken.js";
 
 const userRouter = express.Router();
 
-userRouter.post("/",postUsers);
-userRouter.post("/login",loginUser);
-
+userRouter.post("/", postUsers);
+userRouter.post("/login", loginUser);
+userRouter.put("/:userId/block",authenticateToken, blockUser);
+userRouter.put("/:userId/unblock",authenticateToken, unblockUser);
 
 export default userRouter;
