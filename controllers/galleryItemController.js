6a4563048d448f95cb96d7789c18c57d85374841
Tharu <154546452+TharushiBhagya@users.noreply.github.com>
2024-12-07 21:58:40 +0530

    enhance the galleryController.js code

--- controllers/galleryItemController.js
+++ controllers/galleryItemController.js
@@ -1,39 +1,120 @@
-import GalleryItem from "../models/galleryItem.js";
-import { isAdminValid } from "./userControllers.js";
+import GalleryItem from "../models/galleryItem.js"
 
-export function createGalleryItem(req, res) {
-   
-    if (!isAdminValid(req)) {
-        return res.status(403).json({
-            message: "You are not authorized to create a gallery item"
-        });
+
+export function createGalleryItem(req,res){
+  
+  const user = req.user
+
+  if(user == null){
+
+    res.status(403).json({
+      message : "Please login to create a gallery item"
+    })
+    return
+  }
+
+  if(user.type != "admin"){
+    res.status(403).json({
+      message : "You are not authorized to create a gallery item"
+    })
+    return
+  }
+  
+
+  const galleryItem = req.body
+  const newGalleryItem = new GalleryItem(galleryItem)
+  newGalleryItem.save().then(
+    ()=>{
+      res.json({
+        message : "Gallery Item created successfully"
+      })
     }
+  ).catch(
+    ()=>{
+      res.status(500).json({
+        message : "Gallery Item creation failed"
+      })
+    }
+  )
+}
+export function getGalleryItems(req,res){
 
-    const galleryItem = req.body.item;
-    const newGalleryItem = new GalleryItem(galleryItem);
-
-    newGalleryItem.save()
-        .then(() => {
-            res.json({
-                message: "Gallery item created successfully"
-            });
-        })
-        .catch(() => {
-            res.status(500).json({
-                message: "Gallery item creation failed"
-            });
-        });
+  GalleryItem.find().then(
+    (list)=>{
+      res.json({
+        list : list
+      })
+    }
+  )
 }
 
-export function getGalleryItems(req, res) {
-    GalleryItem.find().then((list) => {
-            res.json({
-                list: list
-            });
-        })
-        .catch(() => {
-            res.status(500).json({
-                message: "Failed to retrieve gallery items"
-            });
-        });
+export function deleteGalleryItem(req,res){
+  const id = req.params.id
+  const user = req.user
+
+  if(user == null){
+
+    res.status(403).json({
+      message : "Please login to create a gallery item"
+    })
+    return
+  }
+
+  if(user.type != "admin"){
+    res.status(403).json({
+      message : "You are not authorized to create a gallery item"
+    })
+    return
+  }
+
+  GalleryItem.findByIdAndDelete(id).then(
+    ()=>{
+      res.json({
+        message : "Gallery Item deleted successfully"
+      })
+    }
+  ).catch(
+    ()=>{
+      res.status(500).json({
+        message : "Gallery Item deletion failed"
+      })  
+    }
+  )
+
+}
+
+export function updateGalleryItem(req,res){
+  const id = req.params.id
+  const user = req.user
+
+  if(user == null){
+
+    res.status(403).json({
+      message : "Please login to create a gallery item"
+    })
+    return
+  }
+
+  if(user.type != "admin"){
+    res.status(403).json({
+      message : "You are not authorized to create a gallery item"
+    })
+    return
+  }
+
+  const galleryItem = req.body
+
+  GalleryItem.findByIdAndUpdate(id,galleryItem).then(
+    ()=>{
+      res.json({
+        message : "Gallery Item updated successfully"
+      })
+    }
+  ).catch(
+    ()=>{
+      res.status(500).json({
+        message : "Gallery Item update failed"
+      })
+    }
+  )
 }
