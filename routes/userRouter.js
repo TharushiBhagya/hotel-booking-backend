73a80bda552cbe2c85c68f963def5f84e6ca8917
Tharu <154546452+TharushiBhagya@users.noreply.github.com>
2024-12-07 21:27:30 +0530

    update user routes

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,11 +1,12 @@
-import express from 'express';
-import { postUsers, loginUser, blockUser, unblockUser } from '../controllers/userController.js';
+import express from "express";
+import { postUsers, loginUser, blockUser, unblockUser, getUser } from '../controllers/userController.js';
 import { authenticateToken } from "../middleware/authenticateToken.js";
 
 const userRouter = express.Router();
 
 userRouter.post("/", postUsers);
 userRouter.post("/login", loginUser);
+userRouter.get("/",getUser);
 userRouter.put("/:userId/block",authenticateToken, blockUser);
 userRouter.put("/:userId/unblock",authenticateToken, unblockUser);
 
